098ae9f8256128705138962cd0db5f45d4b25f99
tehya-laughlin <tehyanlaugh.shop@gmail.com>
2023-09-19 16:59:51 -0600

    Initial Commit

---
+++ APICallsFruit/APICallsFruitApp.swift
@@ -0,0 +1,17 @@
+//
+//  APICallsFruitApp.swift
+//  APICallsFruit
+//
+//  Created by Tehya Laughlin on 9/19/23.
+//
+
+import SwiftUI
+
+@main
+struct APICallsFruitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
